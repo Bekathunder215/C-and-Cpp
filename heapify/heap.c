65990c3d1a1ebc5534c9be62acdc2e65e8bcd59a
Bekathunder215 <johnbekiaris13@gmail.com>
2023-12-22 15:02:45 +0100

    fixed heap bug

--- heapify/heap.c
+++ heapify/heap.c
@@ -1,74 +1,73 @@
 #include<stdio.h>
-#include<stdlib.h>
 
-int pinakas[10];
+int array[10];
 int Hsize;
 char c;
 
-void print_array()
+void print_array() //prints array
 {
 		int j;
-		printf("\no pinakas apoteleitai apo:\n");
+		printf("\nArray consists of:\n");
 		for(j=0;j<=9;j++)
 		{
-			printf("A[%d]=%d \n",j,pinakas[j]);
+			printf("A[%d]=%d \n",j,array[j]);
 		}
 }
 
-void insert_heap()
+void insert_heap() //inserts numbers in the heap
 {
 	int i,k;
-	printf("\nvale 10 nea stoixeia ston pinaka\n");
+	printf("\nInsert 10 numbers in the array\n");
 	for(i=0;i<=9;i++)
 	{
 		scanf("%d",&k);
-		pinakas[i]=k;
-		printf("\nto neo stoixeio pou evales einai to: %d\n",pinakas[i]);
+		array[i]=k;
+		printf("\nYou inserted: %d\n",array[i]);
 	}
 	Hsize=9;
 }
 
-void heapify(int i)
+void heapify(int i) //reorganises the numbers in a heap data structure recursively
 {
 	int l,k,r,largest;
 	l=2*i+1;
 	r=l+1;
-	if((l<=Hsize)&&(pinakas[l]>pinakas[i]))
+	if((l<=Hsize)&&(array[l]>=array[i]))
 		largest=l;
 	else
 		largest=i;
-	if((r<=Hsize)&&(pinakas[r]>pinakas[largest]))
+	if((r<=Hsize)&&(array[r]>array[largest]))
 		largest=r;
 	if(largest!=i)
 	{
-		k=pinakas[i];
-		pinakas[i]=pinakas[largest];
-		pinakas[largest]=k;
+		k=array[i];
+		array[i]=array[largest];
+		array[largest]=k;
 		heapify(k);
 	}
 }
 
-void build_heap()
+void build_heap() //builds heap with heapify function
 {
 	int m;
-	for(m=10;m>=0;m--)
+	for(m=0;m<=Hsize;m++)
 	{
 		heapify(m);
 	}
 }
 
-void heapsort()
+void heapsort() //sorts and prints the array
 {
 	int q,p;
-	for(q=9;q>=0;q--)
+	for(q=Hsize;q>=0;q--)
 	{
-		p=pinakas[0];
-		pinakas[0]=pinakas[q];
-		pinakas[q]=p;
+		p=array[0];
+		array[0]=array[q];
+		array[q]=p;
 		Hsize=Hsize-1;
 		heapify(0);
 	}
-	printf("\no pinakas taksinomithike");
+	printf("\nArray has been sorted");
 	print_array();
 }
 
@@ -77,22 +76,34 @@ int main(void)
 	char c='a';
 	while (c!='q'||c!='Q')
 	{
+		printf("------------------------------------------------");
 		printf("\nPress: \n\ti to Insert.\n\tb to Build heap.\n\ts to Heapsort.\n\tp to Print.\n\tq to Quit.\n");
 		fflush(stdin);
 		scanf("%c",&c);
-		if(c=='i'||c=='I')
-			insert_heap();
-		if(c=='b'||c=='B')
-			build_heap();
-		if(c=='s'||c=='S')
-			heapsort();
-		if(c=='p'||c=='P')
-			print_array();
-		if(c=='q'||c=='Q')
-			{
-				printf("\nGoodbye");
+		switch(c)
+		{
+			case 'i':
+			case 'I':
+				insert_heap();
+				break;
+			case 'b':
+			case 'B':
+				build_heap();
+				break;
+			case 's':
+			case 'S':
+				heapsort();
+				break;
+			case 'p':
+			case 'P':
+				print_array();
+				break;
+			case 'q':
+			case 'Q':
 				return 0;
-			}
+			default:
+				printf("\nPlease insert one of the characters provided\n");
+		}
 	}
 	return 0;
 }
